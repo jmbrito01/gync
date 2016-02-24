7a05bf880cf11f025f9961b3439aea6d7a08dd7e
Joao Marcelo Brito <joaomarcelobrito01@gmail.com>
2016-02-24 02:13:18 -0300

    Fetch tests separed

--- test/run.js
+++ test/run.js
@@ -2,35 +2,6 @@ var Gync = require('../');
 var fs = require('fs');
 
 describe('Gync', function() {
-	describe('#fetch', function() {
-		it('should handle all array promises synchronously', function(done) {
-			Gync.run(function* () {
-				function promiseTest(arg) {
-					return new Promise(function(resolve) {
-						setTimeout(function() {
-							resolve(arg);
-						}, 20);
-					});
-				}
-
-				var result = Gync.fetch([
-					promiseTest(true), promiseTest(true), promiseTest(true)
-				]);
-				return result;
-			}).then(onResolve,  onReject);
-
-			function onResolve(result) {
-				var results = true;
-				for (each of result) if (!each) results = false;
-				if (results) done();
-			}
-
-			function onReject(error) {
-				throw error;
-			}
-		});
-	});
-
 	describe('#run', function() {
 		it('should trigger reject if no generator specified', function(done) {
 			Gync.run().then(onResolve, onReject);
